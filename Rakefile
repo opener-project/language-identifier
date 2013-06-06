4144dcb3c2df90f84ee4005bf97c12af95b34038
Yorick Peterse <yorickpeterse@olery.com>
2013-06-06 19:04:39 +0200

    Compile Perl modules from RubyGems.
    One might wonder: what where they smoking that day? The answer: Ruby.
    
    Signed-off-by: Yorick Peterse <yorickpeterse@olery.com>

--- Rakefile
+++ Rakefile
@@ -1,11 +1,1 @@
-require 'rubygems/package_task'
-require 'rake/extensiontask'
-
-GEMSPEC = Gem::Specification.load('Vicom-language-identifier-lite_kernel.gemspec')
-
-Rake::ExtensionTask.new('language_identifier', GEMSPEC)
-
-Gem::PackageTask.new(GEMSPEC) do |pkg|
-  pkg.need_tar = false
-  pkg.need_zip = false
-end
+require 'bundler/gem_tasks'
--- ext/language_identifier/extconf.rb
+++ ext/language_identifier/extconf.rb
@@ -1,11 +1,22 @@
 require 'mkmf'
 require 'fileutils'
 
-source = File.expand_path('../Makefile_hack', __FILE__)
-target = File.join(Dir.pwd, 'Makefile')
-dummy  = File.join(Dir.pwd, 'language_identifier.so')
+find_executable('perl')
 
-FileUtils.cp(source, target)
-FileUtils.touch(dummy)
+vendor   = File.expand_path('../../../core/vendor', __FILE__)
+perl_lib = File.expand_path('../../../core/lib', __FILE__)
+sanity   = File.join(Dir.pwd, 'language_identifier.so')
+
+FileUtils.touch(sanity)
+
+Dir.glob(File.join(vendor, '*')).each do |dir|
+  Dir.chdir(dir) do
+    puts "Building Perl for #{File.basename(dir)}"
+
+    system("perl Makefile.PL PREFIX=#{perl_lib} LIB=#{perl_lib}")
+    system("make")
+    system("make install")
+  end
+end
 
 $makefile_created = true
