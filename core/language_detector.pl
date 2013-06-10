98c3b60cbe552ac9e537177d2b42b13bda032d8a
Yorick Peterse <yorickpeterse@olery.com>
2013-06-10 12:21:17 +0200

    Use the correct exit statuses in Perl.
    Signed-off-by: Yorick Peterse <yorickpeterse@olery.com>

--- core/language_detector.pl
+++ core/language_detector.pl
@@ -15,58 +15,64 @@ my $HELP = 0;
 my $SUPPORT = 0;
 
 if (scalar(@ARGV) > 0) {
-	for (my $i=0; $i < scalar(@ARGV) && $HELP==0; $i++) {
-		if ($ARGV[$i] eq "--help") {
-                	$HELP = 1;
-                }
-		elsif ($ARGV[$i] eq "-d") {
-                	$SUPPORT = 1;
-                }
-	}
+    for (my $i=0; $i < scalar(@ARGV) && $HELP==0; $i++) {
+        if ($ARGV[$i] eq "--help") {
+            $HELP = 1;
+        }
+        elsif ($ARGV[$i] eq "-d") {
+            $SUPPORT = 1;
+        }
+    }
 }
 
 if ($HELP == 1) {
-	displayHelp();
-} else {
-	my $text ="";
-	while(<STDIN>) {
-		$text .= $_;
-        }
-	#print "$text";
-	#my $file = $ARGV[0];
-	my $guesser;
-#print "support: $SUPPORT\n";
-	if ($SUPPORT == 1) {
-    $guesser = Text::Language::Guess->new();
-	} else {
-    $guesser = Text::Language::Guess->new(languages => ['en', 'fr', 'es', 'it', 'de', 'nl']);
-	}
-        my $lang = $guesser->language_guess_string($text);
-	if ($lang) {
-		print "$lang\n";
-	} else {
-		print "Error: unknown language.";
-	}
+    displayHelp();
+}
+else {
+    my $text ="";
+
+    while(<STDIN>) {
+        $text .= $_;
+    }
+
+    my $guesser;
+
+    if ($SUPPORT == 1) {
+        $guesser = Text::Language::Guess->new();
+    }
+    else {
+        $guesser = Text::Language::Guess->new(languages => ['en', 'fr', 'es', 'it', 'de', 'nl']);
+    }
+
+    my $lang = $guesser->language_guess_string($text);
+
+    if ($lang) {
+        print "$lang\n";
+    }
+    else {
+        print STDERR "Error: unknown language.";
+        exit 1;
+    }
 
 }
 
 ##
 # NOTE: the wrapper around this script actually hijacks the help output, which
-# meanse that the message below never get's displayed.
+# means that the message below never get's displayed.
 #
-# In orde to change the help output check the file:
+# In order to change the help output check the file:
 # lib/opener/language-identifier/option_parser.rb
 #
 sub displayHelp {
-  print STDERR "\nThis aplication reads a text from standard input in order to identify language.\n\n";
-  print STDERR "Usage: cat english.txt | language-identifier\n\n";
-  print STDERR "-d,             (optional) extends language detection,\n";
-  print STDERR "                without this flag application detects:\n";
-  print STDERR "                english (en), french (fr), spanish (es), italian (it),\n";
-  print STDERR "                german (de) and dutch (nl).\n";
-  print STDERR "                with this flag application detects:\n";
-  print STDERR "                english (en), french (fr), spanish (es), portugese (pt),\n";
-  print STDERR "                italian (it), german (de), dutch (nl), swedish (sv),\n";
-  print STDERR "                norwegian (no) and danish (da).\n";
-  print STDERR "--help,         outputs aplication help.\n\n";
+    print STDERR "\nThis aplication reads a text from standard input in order to identify language.\n\n";
+    print STDERR "Usage: cat english.txt | language-identifier\n\n";
+    print STDERR "-d,             (optional) extends language detection,\n";
+    print STDERR "                without this flag application detects:\n";
+    print STDERR "                english (en), french (fr), spanish (es), italian (it),\n";
+    print STDERR "                german (de) and dutch (nl).\n";
+    print STDERR "                with this flag application detects:\n";
+    print STDERR "                english (en), french (fr), spanish (es), portugese (pt),\n";
+    print STDERR "                italian (it), german (de), dutch (nl), swedish (sv),\n";
+    print STDERR "                norwegian (no) and danish (da).\n";
+    print STDERR "--help,         outputs aplication help.\n\n";
 }
