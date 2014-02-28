cfcb48c2e9faf72e0915b60b8db462c83896c0c4
Wilco van Duinkerken <wilco@sparkboxx.com>
2014-02-28 15:57:59 +0100

    hard coded profiles

--- lib/opener/language_identifier/detector.rb
+++ lib/opener/language_identifier/detector.rb
@@ -8,7 +8,7 @@ module Opener
       include Singleton
 
       def initialize
-        @detector = CybozuDetector.new("core/target/classes/profiles")
+        @detector = CybozuDetector.new(profiles_path)
         @semaphore = Mutex.new
       end
 
@@ -16,6 +16,10 @@ module Opener
         @semaphore.synchronize do
           @detector.detect(input)
         end
+      end
+
+      def profiles_path
+        File.expand_path("../../../../core/target/classes/profiles", __FILE__)
       end
 
     end
--- lib/opener/language_identifier/version.rb
+++ lib/opener/language_identifier/version.rb
@@ -1,5 +1,5 @@
 module Opener
   class LanguageIdentifier
-    VERSION = "2.1.0"
+    VERSION = "2.2.0"
   end
 end
