81212f355331725b3fb68e38aa1803919e94ada8
Yorick Peterse <yorickpeterse@olery.com>
2014-05-14 14:30:40 +0200

    Cleaned up daemon bootstrapping code.

--- exec/language-identifier.rb
+++ exec/language-identifier.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
-#
-require 'rubygems'
+
 require 'opener/daemons'
-require 'opener/language_identifier'
+require_relative '../lib/opener/language_identifier'
 
 options = Opener::Daemons::OptParser.parse!(ARGV)
-daemon = Opener::Daemons::Daemon.new(Opener::LanguageIdentifier, options)
+daemon  = Opener::Daemons::Daemon.new(Opener::LanguageIdentifier, options)
+
 daemon.start
